044b5407ccf4dc5529b4fdbb21657b17ee84382d
Jhonata Antunes <jhonata.antunes@controlid.com.br>
2016-12-12 07:25:07 -0200

    Client and server classes

--- Chat/app/src/main/java/com/example/chat/activitys/ChatActivity.java
+++ Chat/app/src/main/java/com/example/chat/activitys/ChatActivity.java
@@ -1,13 +1,15 @@
-package com.example.chat;
+package com.example.chat.activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.chat.R;
+
+public class ChatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_chat);
     }
 }
